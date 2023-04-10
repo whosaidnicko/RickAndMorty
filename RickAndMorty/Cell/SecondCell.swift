6a2c6afcea2ad71027519196455d3eb7974a530b
whosaidnicko <nicu161299@gmail.com>
2023-04-10 12:58:32 +0100

    Complete correct constraints for elements of tableView.

--- RickAndMorty/Cell/SecondCell.swift
+++ RickAndMorty/Cell/SecondCell.swift
@@ -41,7 +41,7 @@ class SecondCell: UITableViewCell {
     }
     func setLabel() {
         
-        nameHero.layer.frame = CGRect(x: 145, y: 12, width: 300, height: 30)
+        nameHero.layer.frame = CGRect(x: 145, y: 24, width: 300, height: 30)
         nameHero.textColor = #colorLiteral(red: 1, green: 0.6838926673, blue: 0, alpha: 1)
 
         
@@ -52,7 +52,7 @@ class SecondCell: UITableViewCell {
     }
     
     func setImage() {
-        pictureHero.layer.frame = CGRect(x: 25, y: 4, width: 114, height: 114)
+        pictureHero.layer.frame = CGRect(x: 25, y: 16, width: 114, height: 114)
         pictureHero.layer.masksToBounds = true
         pictureHero.layer.cornerRadius = 10
         pictureHero.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
@@ -61,7 +61,7 @@ class SecondCell: UITableViewCell {
     
     
     func setShadow() {
-        shadow.layer.frame = CGRect(x: 32, y: 7, width: 320 , height: 108)
+        shadow.layer.frame = CGRect(x: 32, y: 19, width: 320 , height: 108)
         shadow.layer.masksToBounds = false
         shadow.layer.shadowOffset = CGSizeMake(0, 0)
         shadow.layer.shadowColor = UIColor.black.cgColor
@@ -71,7 +71,7 @@ class SecondCell: UITableViewCell {
         addSubview(shadow)
     }
     func backMainViewSet() {
-        backMainView.layer.frame = CGRect(x: 25, y: 4, width: 330, height: 114)
+        backMainView.layer.frame = CGRect(x: 25, y: 16, width: 330, height: 114)
         backMainView.backgroundColor = .white
         backMainView.layer.cornerRadius = 10
         backMainView.layer.masksToBounds = true
@@ -79,7 +79,7 @@ class SecondCell: UITableViewCell {
     }
     
     func setLabelLocation(){
-        labelLocation.layer.frame = CGRect(x: 145, y: 32, width: 250, height: 30)
+        labelLocation.layer.frame = CGRect(x: 145, y: 44, width: 250, height: 30)
         labelLocation.textColor = .systemGray
         labelLocation.font = .boldSystemFont(ofSize: 10)
         addSubview(labelLocation)
@@ -88,14 +88,14 @@ class SecondCell: UITableViewCell {
     
     func setStaticLabelEp() {
         staticLabelEpisode.text = "Episode:"
-        staticLabelEpisode.layer.frame = CGRect(x: 145, y: 23, width: 80, height: 80)
+        staticLabelEpisode.layer.frame = CGRect(x: 145, y: 35, width: 80, height: 80)
         staticLabelEpisode.textColor = .systemGray
         staticLabelEpisode.font = .boldSystemFont(ofSize: 10)
         addSubview(staticLabelEpisode)
         
     }
     func setDinamicLabelEp() {
-        dinamicLabelEpisode.layer.frame = CGRect(x: 145, y: 35, width: 170, height: 80)
+        dinamicLabelEpisode.layer.frame = CGRect(x: 145, y: 47, width: 170, height: 80)
         
         
         dinamicLabelEpisode.textColor = .systemGray
